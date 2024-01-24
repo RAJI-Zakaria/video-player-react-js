0e64186be281885b01c03767ae5e4aa5a1fa1712
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-01-24 15:00:46 +0100

    Setting up the Bootstrap library

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, Col, Row } from 'react-bootstrap';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <Container>
+      <Row>
+      <Col sm={6}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </Col>
+      <Col sm={6}>
+      <h1>Redeemers Salute</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +31,9 @@ function App() {
           Reset
         </button>
       </div>
-    </>
+      </Col>
+      </Row>
+    </Container>
   )
 }
 
