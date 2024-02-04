8f1b092899823554b23e8718b5bfa1feef5e630c
Ferhat SAIDOUN <ferhatte10@gmail.com>
2024-02-04 04:58:14 +0100

    mis en place les composant

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Container, Col, Row } from 'react-bootstrap';
 import Player from './components/Player/Player';
